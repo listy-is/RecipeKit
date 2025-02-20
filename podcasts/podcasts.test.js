15d7bab335eba8cd5ee90fad284604508c07dc2b
NataliaAlv <natalia.alvarez@secture.com>
2025-02-20 13:00:24 +0100

    Updated test podcasts

--- podcasts/podcasts.test.js
+++ podcasts/podcasts.test.js
@@ -1,5 +1,9 @@
 import { expect, test, describe } from "bun:test";
-import { runEngine, findEntry } from '../Engine/utils/test_utils.js';
+import { runEngine, findEntry, loadEnvVariables } from '../Engine/utils/test_utils.js';
+
+// Add process.env variables from the .env file
+await loadEnvVariables();
+const TIMEOUT = parseInt(process.env.TEST_TIMEOUT);
 
 const RECIPE = "apple.json";
 const INPUT = {
@@ -21,7 +25,7 @@ describe(RECIPE, () => {
         expect(entry.SUBTITLE).toBe(ENTRY.SUBTITLE);
         expect(entry.COVER).toMatch(/^https:\/\/.*\.(jpg|jpeg|png|webp)$/i);
         expect(entry.URL).toMatch(/^https:\/\/podcasts\.apple\.com\/us\/podcast\/estirando-el-chicle\/id1511396938(\/?|\?.*)?$/i);
-    });
+    }, TIMEOUT);
 
     test("--type url", async () => {
         const result = await runEngine (`podcasts/${RECIPE}`, "url", INPUT.URL);
@@ -33,5 +37,5 @@ describe(RECIPE, () => {
         expect(result.GENRE).toBeDefined();
         expect(result.PRICE).toBeDefined();
         expect(result.COVER).toMatch(/^https:\/\/.*\.(jpg|jpeg|png|webp)$/i);
-    });
+    }, TIMEOUT);
 });
