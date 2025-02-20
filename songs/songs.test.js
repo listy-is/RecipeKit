e38ea314736dd00cfc93b70db19aa0bc28fdd745
NataliaAlv <natalia.alvarez@secture.com>
2025-02-20 13:00:55 +0100

    Updated test songs

--- songs/songs.test.js
+++ songs/songs.test.js
@@ -1,5 +1,9 @@
 import { expect, test, describe } from "bun:test";
-import { runEngine, findEntry } from '../Engine/utils/test_utils.js';
+import { runEngine, findEntry , loadEnvVariables } from '../Engine/utils/test_utils.js';
+
+// Add process.env variables from the .env file
+await loadEnvVariables();
+const TIMEOUT = parseInt(process.env.TEST_TIMEOUT);
 
 const RECIPE = "apple.json";
 const INPUT = {
@@ -19,7 +23,7 @@ describe(RECIPE, () => {
 		expect(entry.SUBTITLE).toBe(ENTRY.SUBTITLE);
 		expect(entry.COVER).toMatch(/^https:\/\/.*\.(jpg|jpeg|png|webp)$/i);
 		expect(entry.URL).toMatch(/^https:\/\/itunes\.apple\.com\/.+/i);
-	})
+	}, TIMEOUT);
 
 	test("--type url", async () => {
 		const result = await runEngine(`songs/${RECIPE}`, "url", INPUT.URL);
@@ -30,5 +34,5 @@ describe(RECIPE, () => {
 		expect(result.GENRE).toBeDefined();
 		expect(result.PRICE).toBeDefined();
 		expect(result.COVER).toMatch(/^https:\/\/.*\.(jpg|jpeg|png|webp)$/i);
-	});
+	}, TIMEOUT);
 });
